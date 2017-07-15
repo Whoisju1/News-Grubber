a7de2b2fdd4c286c63e95d3011bca1a762f65b70
Juan <whoisju1@gmail.com>
2017-07-14 21:18:07 -0400

    set up  mongoose connection

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require('mongoose');
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+//connect to mongoose server
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/News-Grubber", {
+    useMongoClient: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("Mongoose connected successfully"));
+
+//set view engine
 
 app.listen(port, () => console.log(`Listening on PORT: ${port}`));
