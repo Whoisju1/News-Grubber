48d492aaa0b4f339f0b35d8e0f98761319250cc0
Juan <whoisju1@gmail.com>
2017-07-14 22:23:49 -0400

    get root route working

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const request = require('request');
+const cheerio = require('cheerio');
+const exphbs  = require('express-handlebars');
+const fs = require('fs');
+const logger = require('logger');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+    let now = Date().toString();
+    let log = `${now} | ${req.method} : ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        console.log('Unable to append to server.log');
+    });
+    next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+// hbs.registerPartials(__dirname + '/views/partials');
+//set view engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 //connect to mongoose server
 mongoose.Promise = global.Promise;
@@ -13,6 +35,8 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => console.log("Mongoose connected successfully"));
 
-//set view engine
+// import routes 
+app.use(require('./controller'));
+
 
 app.listen(port, () => console.log(`Listening on PORT: ${port}`));
