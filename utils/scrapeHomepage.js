3b98e0a77ea301d3a777a6715f41ad7c7aa98513
Juan John-Charles <whoisju1@gmail.com>
2018-03-31 16:35:55 -0400

    create function that scrapes homepage

---
+++ utils/scrapeHomepage.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+module.exports = () => new Promise((resolve, reject) => {
+  const rootURL = 'https://www.cnet.com';
+  request(rootURL, (err, response, body) => {
+    if (err) reject(err);
+    const $ = cheerio.load(body);
+
+    const linkedItems = [];
+
+    $('section div.item')
+      .each(function each() {
+        // grab needed information from specified element
+        const title = $(this).find('h3').text();
+        const subTitle = $(this).find('p').text();
+        const image = $(this).find('figure > a > span > img').attr('src');
+        const url = `${rootURL}${$(this).find('span > a').attr('href')}`;
+
+        // only push into array if all the information is available
+        if (title && subTitle && image) {
+          linkedItems.push({
+            title, subTitle, image, url,
+          });
+        }
+      });
+
+    resolve(linkedItems);
+  });
+});
+
