3c5cd851594703353530486cbf9ff323cdf41c26
Juan John-Charles <whoisju1@gmail.com>
2018-04-02 00:32:28 -0400

    set up mongoose

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.set('debug', true);
+mongoose.Promise = Promise;
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:news-grubber';
+
+mongoose.connect(mongoURI, {
+  keepAlive: true,
+});
