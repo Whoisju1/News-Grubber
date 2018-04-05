57514811674e8a8af2c5a482221d41ee807a19f5
Juan John-Charles <whoisju1@gmail.com>
2018-04-05 12:43:23 -0400

    set up signin configeration

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,7 +1,46 @@
 const db = require('../models');
 const jwt = require('jsonwebtoken');
 
-exports.SignIn = function (req, res, next) {}; // eslint-disable-line
+exports.SignIn = async function (req, res, next) {
+  try {
+    // find user
+    const user = await db.User.findOne({
+      username: req.body.username,
+    });
+    // if no user exists send back an error message of "User not found"
+    if (!user) return next({ status: 401, message: 'User not found' });
+    const { id = null, username = null, profileImageURL = null } = user;
+    // if user exits check to see if the password sent to the server matches
+    const isMatched = await user.comparePassword(req.body.password);
+    // if password matches send back user information with a token
+    if (isMatched) {
+      const token = jwt.sign(
+        {
+          id,
+          username,
+          profileImageURL,
+        },
+        process.env.SECRETE_KEY,
+      );
+      return res.status(200).json({
+        id,
+        username,
+        profileImageURL,
+        token,
+      });
+    }
+    // if something doesn't match send an error
+    return next({
+      status: 400,
+      message: 'Invalid password.',
+    });
+  } catch (err) {
+    return next({
+      status: 400,
+      message: 'Oops! Something went wrong.',
+    });
+  }
+};
 
 exports.signUp = async function signUp(req, res, next) {
   try {
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const { signUp } = require('../handlers/auth');
+const { signUp, SignIn } = require('../handlers/auth');
 
 router.post('/signup', signUp);
+router.post('/signin', SignIn);
 
 module.exports = router;
