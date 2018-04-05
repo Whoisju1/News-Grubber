be8bbeb0bc83e47ea028ac9602ea68cdf058f42a
Juan John-Charles <whoisju1@gmail.com>
2018-04-05 10:38:56 -0400

    clean up errors and typos on mongoose index

--- models/index.js
+++ models/index.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 mongoose.Promise = Promise;
-const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:news-grubber';
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost/newsgrubber';
 
 mongoose.connect(mongoURI, {
   keepAlive: true,
 });
+
+module.exports.User = require('./user');
