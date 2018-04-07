9245cf3c2f08f469ed956323bd484d326337b344
Juan John-Charles <whoisju1@gmail.com>
2018-04-07 17:42:25 -0400

    set up function to delete note from article

--- handlers/notes.js
+++ handlers/notes.js
@@ -12,5 +12,16 @@ exports.addNote = async (req, res, next) => {
     return next(e);
   }
 };
-exports.deleteNote = async (req, res, next) => {}; // eslint-disable-line no-unused-vars
+exports.deleteNote = async (req, res, next) => {
+  try {
+    const { id: userID } = req.params;
+    const { articleID, noteID } = req.body;
+    const foundArticle = await db.Article.findById(articleID).where({ user: userID });
+    await foundArticle.notes.id(noteID).remove();
+    const reducedArticle = await foundArticle.save();
+    return res.status(200).json(reducedArticle);
+  } catch (e) {
+    return next(e);
+  }
+};
 exports.editNote = async (req, res, next) => {}; // eslint-disable-line no-unused-vars
