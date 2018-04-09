9ab58a60ed39bb7f080e2a3b999afa75240607f8
Juan John-Charles <whoisju1@gmail.com>
2018-04-09 15:13:18 -0400

    set up files for handlebars

--- handlers/home.js
+++ handlers/home.js
@@ -3,7 +3,6 @@ const scrapedData = require('../utils/getArticleInfo');
 const home = async (req, res, next) => {
   try {
     const article = await scrapedData;
-    console.log(article);
     return res.render('index', { article });
   } catch (e) {
     return next(e);
