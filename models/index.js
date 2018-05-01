32b89adffaf04bb9eaaad72d9de585fe33aa380c
Juan John-Charles <whoisju1@gmail.com>
2018-04-30 21:23:54 -0400

    move database uri to env file

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 mongoose.Promise = Promise;
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/newsgrubber';
+const mongoURI = process.env.MONGODB_URI;
 
 mongoose.connect(mongoURI, {
   keepAlive: true,
