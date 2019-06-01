031810d37ee3c07206dcc291bd9429bda360b861
Juan John-Charles <whoisju1@gmail.com>
2019-06-01 17:25:30 -0400

    write tests for sccapeHompage funtion

--- src/utils/scrapeHomepage/scrapeHomepage.unit.test.js
+++ src/utils/scrapeHomepage/scrapeHomepage.unit.test.js
@@ -1,9 +1,22 @@
+import '@babel/polyfill';
 import scrapeHomepage from './scrapeHomepage';
 
 describe('scapeHomepage', () => {
   describe('when called', () => {
     it('should return a promise', () => {
       expect(scrapeHomepage()).toBeInstanceOf(Promise);
+    });
+
+    it('should be resolved with an array', async () => {
+      const data = await scrapeHomepage();
+      expect(Array.isArray(data)).toBe(true);
+    });
+
+    it('that has an objects each with keys of `title`, `subTitle` and `image`', async () => {
+      const data = await scrapeHomepage();
+      expect(data[0]).toHaveProperty('title');
+      expect(data[0]).toHaveProperty('subTitle');
+      expect(data[0]).toHaveProperty('image');
     });
   });
 });
