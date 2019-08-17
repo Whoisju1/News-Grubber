d2e36e807ccd839f5f53d2e4298874f83530a64a
Juan John-Charles <whoisju1@gmail.com>
2019-08-17 16:10:12 -0400

    change color of button to a gradient

--- frontend/src/pages/Articles/Article/SaveBtn/SaveBtn.tsx
+++ frontend/src/pages/Articles/Article/SaveBtn/SaveBtn.tsx
@@ -4,9 +4,12 @@ import styled from 'styled-components';
 const Button = styled.button`
   border: none;
   color: #ffffff;
-  background-color: var(--primary-color);
+  background-image: linear-gradient(50deg, var(--primary-color), #eb685a);
   cursor: pointer;
   transition: all .2s linear;
+  border-radius: 4px;
+  text-transform: uppercase;
+  letter-spacing: 1px;
   &:hover {
     transform: translateY(-2px);
   }
