5be5b7fee51dd6adaa7cfff1f04f9ed6e3169ca4
Juan John-Charles <whoisju1@gmail.com>
2019-10-20 14:11:19 -0400

    remove build from docker-compose script

--- restart.sh
+++ restart.sh
@@ -3,6 +3,6 @@
 clear && printf 'Taking down containers...\n' \
 && docker-compose down \
 && clear && printf 'buidling containers ....\n' \
-&& docker-compose up --build -d \
+&& docker-compose up -d \
 && clear && printf 'containers built successfully\n' \
 && docker-compose logs -f
