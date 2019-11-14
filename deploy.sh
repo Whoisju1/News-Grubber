cda30b18c26a51c491237f65b4c44e25dc93a762
Juan John-Charles <whoisju1@gmail.com>
2019-11-14 17:29:02 -0500

    setup prod dockerfile for client

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml config > compose.yml \
 && cat compose.yml \
-&& docker stack deploy -c compose.yml news-grubber
+&& docker stack deploy -c compose.yml news-grubber \
+&& echo "----------- stack deployed -----------" \
+&& echo "----------- service list -----------" \
+&& docker service ls \
+&& echo "----------- service list -----------" \
+&& docker service ps news-grubber
