4181c117e4f3901e0d65d6e8cf2bb0360d26104a
Juan John-Charles <whoisju1@gmail.com>
2019-12-01 15:48:35 -0500

    refactor start_container script

--- start_containers.sh
+++ start_containers.sh
@@ -1,8 +1,40 @@
 #!/bin/bash/
 
-clear && printf 'Taking down containers...\n' \
-&& docker-compose down \
-&& clear && printf 'buidling containers ....\n' \
-&& docker-compose up -d --build \
-&& clear && printf 'containers built successfully\n' \
-&& docker-compose logs -f
+clear
+
+function printGreenText()
+{
+  printf "\e[32m$*\e[0m \n"
+}
+
+function printRedText()
+{
+  printf "\e[31m$*\e[0m \n"
+}
+
+function start_containers()
+{
+  printf '\e[32m... Taking down containers ...\e[0m \n' \
+  && docker-compose down \
+  && printf '\e[32m... buidling containers ....\e[0m \n' \
+  && docker-compose up -d "$@" \
+  && printf '\e[32m... containers built successfully ...\e[0m \n' \
+  && docker-compose logs -f
+}
+
+printf "\e[35mDo you want to build images? (Y/N)\e[0m"
+
+read answer
+
+if [[ ${answer} =~ ^Yes|yes|y|Y$ ]]
+then
+  printGreenText "You choose to build images."
+  start_containers --build
+elif [[ ${answer} =~ ^No|no|n|N$ ]]
+then
+  printGreenText "You chose not to build images."
+  start_containers
+else
+  printRedText "Answer not recorgnized."
+  exit 1
+fi
