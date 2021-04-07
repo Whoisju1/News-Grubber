88891f006b70558689eb2aa6cce3c8fff3305cf7
Juan John-Charles <whoisju1@gmail.com>
2021-04-07 01:33:05 -0400

    fix unit test script

--- run_tests.sh
+++ run_tests.sh
@@ -4,8 +4,10 @@ export DOCKER_CLIENT_TIMEOUT=120
 export COMPOSE_HTTP_TIMEOUT=120
 
 echo "----- RUNNING UNIT TESTS -----" \
-&& docker-compose -f docker-compose.test.yml up -d test-db-mongo test-db-redis api-base \
-&& docker-compose -f docker-compose.test.yml up --abort-on-container-exit client_unit_test\
-&& docker-compose -f docker-compose.test.yml up --abort-on-container-exit api_unit_tests\
+&& docker-compose -f docker-compose.test.yml up -d \
+&& echo "----- RUNNING CLIENT UNIT TESTS -----" \
+&& docker-compose -f docker-compose.test.yml exec client_unit_test npx react-scripts test --watchAll=false \
+&& echo "----- RUNNING API UNIT TESTS -----" \
+&& docker-compose -f docker-compose.test.yml exec api_unit_tests npm run test:unit \
 && echo "✔✔ All tests passed ✔✔" \
-&& docker-compose down --remove-orphans
+&& docker-compose -f docker-compose.test.yml down --remove-orphans
