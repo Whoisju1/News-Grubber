c4a05a0ebee502082d048af8e9d23c0fba16815b
Juan John-Charles <whoisju1@gmail.com>
2021-04-07 22:04:14 -0400

    add line to test script

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,7 @@ export DOCKER_CLIENT_TIMEOUT=120
 export COMPOSE_HTTP_TIMEOUT=120
 
 echo "----- RUNNING UNIT TESTS -----" \
+&& docker-compose -f docker-compose.test.yml down --remove-orphans \
 && docker-compose -f docker-compose.test.yml up -d \
 && echo "----- RUNNING CLIENT UNIT TESTS -----" \
 && docker-compose -f docker-compose.test.yml run client-unit-test npx react-scripts test --watchAll=false \
